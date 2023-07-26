20cafa18689f54a7fe5bc18b82edf19d33766f88
Juan Aste <juanpabloaste00@gmail.com>
2023-07-26 19:11:08 -0300

    first Commit

---
+++ components/Featured.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import Center from "./Center";
+
+const Bg = styled.div`
+  background-color: #222;
+  color: #fff;
+  padding: 50px 0;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-weight: normal;
+`;
+
+const Desc = styled.p`
+  color: #aaa;
+  font-size: 0.8rem;
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.8fr 1.2fr;
+  gap: 40px;
+  img {
+    max-width: 100%;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export default function Featured() {
+  return (
+    <Bg>
+      <Center>
+        <Wrapper>
+          <Column>
+            <div>
+              <Title>Pro anywhere</Title>
+              <Desc>
+                Lorem Ipsum es simplemente el texto de relleno de las imprentas
+                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+                estándar de las industrias desde el año 1500, cuando un impresor
+                (N. del T. persona que se dedica a la imprenta)
+              </Desc>
+            </div>
+          </Column>
+          <Column>
+            <img src="https://i.ibb.co/LvddDcf/Iphone-14-pro.jpg" />
+          </Column>
+        </Wrapper>
+      </Center>
+    </Bg>
+  );
+}
