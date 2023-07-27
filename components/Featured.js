fe51e766a1cae9637bb9bce62d52b3b447adf8f0
Juan Aste <juanpabloaste00@gmail.com>
2023-07-27 12:38:56 -0300

    Feature Product

--- components/Featured.js
+++ components/Featured.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Center from "./Center";
+import Button from "./Button";
+import ButtonLink from "./ButtonLink";
 
 const Bg = styled.div`
   background-color: #222;
@@ -10,6 +12,7 @@ const Bg = styled.div`
 const Title = styled.h1`
   margin: 0;
   font-weight: normal;
+  font-size: 3rem;
 `;
 
 const Desc = styled.p`
@@ -17,9 +20,10 @@ const Desc = styled.p`
   font-size: 0.8rem;
 `;
 
-const Wrapper = styled.div`
+const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 0.8fr 1.2fr;
+  grid-template-columns: 1.1fr 0.9fr;
+  justify-items: center;
   gap: 40px;
   img {
     max-width: 100%;
@@ -31,26 +35,43 @@ const Column = styled.div`
   align-items: center;
 `;
 
-export default function Featured() {
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 25px;
+`;
+
+export default function Featured({ product }) {
   return (
     <Bg>
       <Center>
-        <Wrapper>
+        <ColumnsWrapper>
           <Column>
             <div>
-              <Title>Pro anywhere</Title>
-              <Desc>
-                Lorem Ipsum es simplemente el texto de relleno de las imprentas
-                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
-                estándar de las industrias desde el año 1500, cuando un impresor
-                (N. del T. persona que se dedica a la imprenta)
-              </Desc>
+              <Title>{product.title}</Title>
+              <Desc>{product.description}</Desc>
+              <ButtonsWrapper>
+                <ButtonLink href={"/products/" + product._id} outline={1} white={1}>
+                  Read more
+                </ButtonLink>
+                <Button primary={1}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  </svg>
+                  Add to cart
+                </Button>
+              </ButtonsWrapper>
             </div>
           </Column>
           <Column>
-            <img src="https://i.ibb.co/LvddDcf/Iphone-14-pro.jpg" />
+            <img src={product.images[0]} alt="" />
           </Column>
-        </Wrapper>
+        </ColumnsWrapper>
       </Center>
     </Bg>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-
-export default function Home() {
+export default function Home({ product }) {
   return (
     <div>
-        <Header />
-        <Featured />
+      <Header />
+      <Featured product={product} />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featureProductId = "64bff097f713bcaebdee5ef7";
+  mongooseConnect();
+  const product = await Product.findById(featureProductId);
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
 }
